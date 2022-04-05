7ce7b7df6631c7f7e6123384a76bdd4b0cad7cd1
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-04-04 19:09:53 -0500

    I don't even know, leave me alone.

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,45 @@
+const startScrn = document.getElementById("startScrn")
 const btn = document.getElementById("btn");
 
-btn.addEventListener("click", ()=>{
+var myQuestions = [
+    {
+        question: "What color is the sky?",
+
+        answers: {
+            a: "red",
+            b: "green",
+            c: "blue"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What color is red?",
+
+        answers: {
+            a: "red",
+            b: "green",
+            c: "blue"
+        },
+        correctAnswer: "a"
+    }
+]
+
+function hideStart() {
+    startScreen.setAttribute("class", "hidden")
+}
+
+function buildQuiz(){}
+
+
+
+btn.onclick = function btnSwitch () {
+
+    console.log("poopoo")
 
     if("btn.innerText === 'START'"){
         btn.innerText = "NEXT";
+        hideStart()
     }
-});
 
+    
+}
