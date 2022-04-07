13cb9860702193470797566ee97d0433df34dbc8
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-04-06 22:09:08 -0500

    appened question on button click

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const startScreen = document.getElementById("startScreen")
 const btn = document.getElementById("btn");
 const subLine = document.getElementById("subLine")
+const quizContainer = document.getElementById('quizContainer')
 
 var myQuestions = [
     {
@@ -22,24 +23,34 @@ var myQuestions = [
             c: "blue"
         },
         correctAnswer: "a"
-    }
+    },
 ]
+function buildQuiz(){
+    for (var i = 0; i < myQuestions.length; i++){
+        // console.log(myQuestions[i].answers)
+        // let quizQuestion = document.createElement('h3')
+        // let optionContainer = document.createElement('button class="btn-outline-primary"')
+        // let quizOption = document.createElement('button class="btn-outline-primary"')
 
-function hideStart() {
-    startScreen.setAttribute("class", "hidden")
-    subLine.setAttribute("class","hidden")
+        // quizQuestion.textContent = myQuestions[i].question
+
+        quizContainer.innerHTML = `<h3>${myQuestions[i].question}</h3>
+        <form action="/action_page.php">
+            <input type="radio" id="a" name="${myQuestions[i].answers} value="a">
+            <label for="a">${myQuestions[i].answers.a}</label><br>
+            <input type="radio" id="b" name="${myQuestions[i].answers} value="b">
+            <label for="b">${myQuestions[i].answers.b}</label><br>
+            <input type="radio" id="c" name="${myQuestions[i].answers} value="c">
+            <label for="c">${myQuestions[i].answers.c}</label><br>
+        </form>`
+    }
+
 }
 
-function buildQuiz(){}
+buildQuiz()
 
-
-
-btn.onclick = function btnSwitch () {
-
-    console.log("start>next")
-
-    if("btn.innerText === 'START'"){
-        btn.innerText = "NEXT";
-        hideStart()
-    }    
+btn.onclick = function hideStart() {
+    startScreen.setAttribute("class", "hidden")
+    subLine.setAttribute("class","hidden")
+    quizContainer.removeAttribute('class', 'hidden')
 }
